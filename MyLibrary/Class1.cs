64fb79d9c48f281e8d371e9baa55ae8d34044ccf
nguyenminhsang2004 <sangnguyencrom@gmail.com>
2020-12-19 01:34:21 +0700

    Commit thử nè

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace MyLibrary
+{
+    public static class Class1
+    {
+        public static void ChoseButton(this Button btn)
+        {
+
+        }
+    }
+}
